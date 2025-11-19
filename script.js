79c138e73462cd0baa6cb88290829eced1d5b01e
Rakibul Islam <rakib.ca@gmail.com>
2025-11-18 22:05:26 -0700

    added feedback list component

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // -- GLOBAL --
+const MAX_CHARS = 150;
+
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
 const submitBtnEl = document.querySelector('.submit-btn');
+const spinnerEl = document.querySelector('.spinner');
 
 // -- COUNTER COMPONENT --
 const inputHandler = () => {
   // determine maximum number of characters
-  const maxNrChars = 150;
+  const maxNrChars = MAX_CHARS;
 
   // determine number of characters currently typed
   const nrCharsTyped = textareaEl.value.length;
@@ -22,7 +25,19 @@ const inputHandler = () => {
 
 textareaEl.addEventListener('input', inputHandler);
 
-// -- SUBMIT COMPONENT --
+// -- FORM COMPONENT --
+const showVisualIndicator = (textCheck) => {
+  const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
+
+  // show valid indicator
+  formEl.classList.add(className);
+
+  // remove visual indicator
+  setTimeout(() => {
+    formEl.classList.remove(className);
+  }, 2000);
+};
+
 const submitHandler = (event) => {
   // prevent default browser action (submitting form data to 'action'-address and refreshing page)
   event.preventDefault();
@@ -32,21 +47,9 @@ const submitHandler = (event) => {
 
   // validate text (e.g. check if #hashtag is present and text is long enough)
   if (text.includes('#') && text.length >= 5) {
-    // show valid indicator
-    formEl.classList.add('form--valid');
-
-    // remove visual indicator
-    setTimeout(() => {
-      formEl.classList.remove('form--valid');
-    }, 2000);
+    showVisualIndicator('valid');
   } else {
-    // show invalid indicator
-    formEl.classList.add('form--invalid');
-
-    // remove visual indicator
-    setTimeout(() => {
-      formEl.classList.remove('form--invalid');
-    }, 2000);
+    showVisualIndicator('invalid');
 
     // focus textarea
     textareaEl.focus();
@@ -92,7 +95,50 @@ const submitHandler = (event) => {
   submitBtnEl.blur();
 
   // reset counter
-  counterEl.textContent = '150';
+  counterEl.textContent = MAX_CHARS;
 };
 
 formEl.addEventListener('submit', submitHandler);
+
+// -- FEEDBACK LIST COMPONENT --
+fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+  .then((response) => response.json())
+  .then((data) => {
+    // remove spinner
+    spinnerEl.remove();
+
+    // iterate over each element in feedbacks array and render it in list
+    data.feedbacks.forEach((feedbackItem) => {
+      // new feedback item HTML
+      const feedbackItemHTML = `
+                <li class="feedback">
+                    <button class="upvote">
+                        <i class="fa-solid fa-caret-up upvote__icon"></i>
+                        <span class="upvote__count">${
+                          feedbackItem.upvoteCount
+                        }</span>
+                    </button>
+                    <section class="feedback__badge">
+                        <p class="feedback__letter">${
+                          feedbackItem.badgeLetter
+                        }</p>
+                    </section>
+                    <div class="feedback__content">
+                        <p class="feedback__company">${feedbackItem.company}</p>
+                        <p class="feedback__text">${feedbackItem.text}</p>
+                    </div>
+                    <p class="feedback__date">${
+                      feedbackItem.daysAgo === 0
+                        ? 'NEW'
+                        : `${feedbackItem.daysAgo}d`
+                    }</p>
+                </li>
+            `;
+
+      // insert new feedback item in list
+      feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+    });
+  })
+  .catch((error) => {
+    feedbackListEl.textContent = `Failed to fetch feedback items. Error message: ${error.message}`;
+  });
